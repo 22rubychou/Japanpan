fedd7e05f7593fc0388383cc19d13864f2c92eed
22rubychou <70134566+22rubychou@users.noreply.github.com>
2021-05-08 19:15:54 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -143,15 +143,5 @@ router.beforeEach((to, from, next) => {
   }
 })
 
-router.afterEach((to, from) => {
-  let title = ''
-  if (to.name === 'Album') {
-    title = store.state.user.name + '的相簿'
-  } else {
-    title = to.meta.title
-  }
-
-  document.title = title
-})
 
 export default router
